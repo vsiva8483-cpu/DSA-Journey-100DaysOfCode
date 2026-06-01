4b28015d3bb5c157efbba345127be1e306edf30e
Venkateswarlu vankaraboina <vsiva8483@gmail.com>
2026-06-01 15:45:44 +0530

    Create Day-31-Minimum-Cost-Of-Buying-Candies-With-Discount.java

---
+++ Leet-Code-Problems/Day-31-Minimum-Cost-Of-Buying-Candies-With-Discount.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int ans=0;
+        for(int i=cost.length-1;i>=0;i-=3){
+            ans=ans+cost[i];
+        if(i-1>=0){
+            ans=ans+cost[i-1];
+        }
+    }
+    return ans;
+    }
+}
