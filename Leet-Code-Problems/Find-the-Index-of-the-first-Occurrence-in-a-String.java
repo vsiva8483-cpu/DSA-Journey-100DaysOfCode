8e73c7d146bd667b115d427cea4f8a29530875c6
Venkateswarlu vankaraboina <vsiva8483@gmail.com>
2026-06-01 16:40:36 +0530

    Create Find-the-Index-of-the-first-Occurrence-in-a-String.java

---
+++ Leet-Code-Problems/Find-the-Index-of-the-first-Occurrence-in-a-String.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i=0; i<=haystack.length()-needle.length();i++){
+            int j;
+            for(j=0;j<needle.length();j++){
+                    if(haystack.charAt(i+j)!=needle.charAt(j)){
+                        break;
+                    }
+            }
+            if(j==needle.length()){
+                return i;
+            }
+        }
+        return-1;
+    }
+}
